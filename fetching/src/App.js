b306a660bbffa69d3249244d1fc532ae526f7c78
Faik Turan <turanfaik@hotmail.com>
2022-10-17 20:25:01 +0300

    fetching app done

---
+++ fetching/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import UsersAxios from './components/UsersAxios';
+//import UsersFetch from './components/UsersFetch';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <UsersFetch /> */}
+      <UsersAxios />
+    </div>
+  );
+}
+
+export default App;
---
+++ fetching/src/components/UsersAxios.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react';
+
+import axios from "axios";
+
+import UserCount from './UserCount';
+
+function UsersAxios() {
+    const [loading, setLoading] = useState(true);
+    const [users, setUsers]=useState([]);
+
+    const [name, setName] = useState("");
+    const [search, setSearch] = useState("");
+
+    useEffect(() => {
+     getUsers();
+    }, []);
+
+    const getUsers = async () => {
+        try {
+            const { data } = await axios.get(
+                "https://jsonplaceholder.typicode.com/users"
+            );
+            setUsers(data);
+            setLoading(false);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    const saveUser = () =>{
+        if(name=== "") return false;
+
+        setUsers([...users, { name }]);
+    };
+
+    const filtered = users.filter((user) => 
+    user.name.toLowerCase().includes(search.toLowerCase()));
+
+  return (
+    <div>
+        <input 
+        name="name"
+        placeholder="New User"
+        value={name}
+        onChange={(e) => setName(e.target.value)} />
+
+        <input type="button" value="Save" onClick={saveUser} />
+
+        <hr />
+        <input 
+            name="filter"
+            placeholder="Filter"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+        />
+
+        {loading ? (
+            <h3>Loading...</h3>
+        ) : (
+            <ul>
+                {filtered.map((user, key) => (
+                    <li className="listItem" key={key}>
+                        {user.name}
+                    </li>
+                ))}
+            </ul>
+        )}
+            <UserCount count={filtered.length} />
+    </div>
+  );
+}
+
+export default UsersAxios;
