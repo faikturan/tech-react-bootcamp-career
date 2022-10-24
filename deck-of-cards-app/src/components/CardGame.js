32913a99866375b44d19e4a7f44b3444be12d3a5
Faik Turan <turanfaik@hotmail.com>
2022-10-24 19:39:31 +0300

    deck of cards app done

--- deck-of-cards-app/src/components/CardGame.js
+++ deck-of-cards-app/src/components/CardGame.js
@@ -1,53 +1,63 @@
-/* eslint-disable no-template-curly-in-string */
-import React, { Component } from 'react';
-
+import React, { Component } from 'react'
 import axios from "axios";
 
-import Card from "./Card"
+import './CardGame.css';
+import Card from "./Card";
 
-class CardGame extends Component {
-
+export class CardGame extends Component {
     state = {
         deckId: '',
-        cardsRemaining: 52,
-        cards:[],
+        cardsRemaining: '52',
+        cards:[]
     }
 
     async componentDidMount(){
         const deck = await axios.get(
-            'https://www.deckofcardsapi.com/api/deck/new/shuffle/'
+            'https://deckofcardsapi.com/api/deck/new/shuffle/'
         )
-        this.setState( { deckId: deck.data.deck_id })
+        this.setState({ deckId: deck.data.deck_id })
     }
 
     getCard = async () => {
         const response = await axios.get(
-            'https://www.deckofcardsapi.com/api/deck/${this.state.deckId}/draw'
+            `https://deckofcardsapi.com/api/deck/${this.state.deckId}/draw/`
         )
 
         const rotate = Math.floor(Math.random() * 81 - 40)
-        const transX = Math.floor(Math.random() * 41 - 20)
         const transY = Math.floor(Math.random() * 41 - 20)
+        const transX = Math.floor(Math.random() * 41 - 20)
         const transformStyle = {
-            transform: `translateX(calc(-50% + ${transX}px))
+            transform: `translateX(calc(-50% + ${transX}px)) 
             translateY(${transY}px) rotate(${rotate}deg)`,
         }
 
-        const newCard = { ...response.data.cards[0], style:transformStyle }
-
+        const newCard = {...response.data.cards[0], style:transformStyle}
         this.setState(prevSt => ({
             cardsRemaining: response.data.remaining,
             cards: [...prevSt.cards, newCard],
         }))
 
+
     }
 
   render() {
+    const { cards, cardsRemaining } = this.state
     return (
-      <div>CardGame</div>
+      <div className="CardGame">
+      <button onClick={this.getCard} disabled={cardsRemaining === 0}>
+        Gimme a Card
+      </button>
+      
+      <p>Cards Remaining: {cardsRemaining}</p>
+      <div className="CardGame__cards">
+      {cards.map(card => (
+        <Card {...card} key={card.code} />
+      ))}
+
+      </div>
+      </div>
     )
   }
 }
 
-
-export default CardGame;
+export default CardGame
