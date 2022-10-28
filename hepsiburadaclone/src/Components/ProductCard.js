b87e51791740e93c5c170194ee3ac6dee2443246
Faik Turan <turanfaik@hotmail.com>
2022-10-28 18:44:16 +0300

    hepsiburada clone contunie

---
+++ hepsiburadaclone/src/Components/ProductCard.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+Card,
+Header,
+Images,
+Price,
+AddToCart,
+} from "../Styles/ProductCard.Style";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.scss";
+import { AddToCart as AddToCartAction } from "../Store/actions/CartActions";
+import slugify from "slugify";
+import { useDispatch } from "react-redux";
+
+const ProductCard = ( { product }) => {
+  const { name, images, price } =  product;
+  const dispatch = useDispatch();
+  return (
+    <Card href={"/" + slugify(name)}>
+      <Header>
+        <span>{name}</span>
+      </Header>
+    <Images>
+      <Swiper style={{ width: "100%" }}>
+      {images.map((image, index) => (
+        <SwiperSlide key={index}>
+        <img 
+          src={image}
+          alt={"preview" + index}
+          style={{ width: "100%", height: "100%"}}
+        />
+        </SwiperSlide>
+      ))}
+      </Swiper>
+    </Images>
+    <Price>
+      <span>{price} TL</span>
+    </Price>
+    <AddToCart
+    onClick={(e) => {
+      e.preventDefault();
+      dispatch(AddToCartAction(product));
+    }}>
+    <span>Sepete Ekle</span>
+    </AddToCart>
+    </Card>
+  );
+};
+
+export default ProductCard;
---
+++ hepsiburadaclone/src/Components/ProductDetails.js
@@ -0,0 +1,112 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { AddToCart } from '../Store/actions/CartActions';
+import {
+Content,
+Details,
+ImageBox,
+Slides,
+PreviewImages,
+ProductName,
+AddToCardRow,
+SellerRow,
+BrandName,
+PriceandRatings,
+} from "../Styles/Product.Style";
+
+const ProductDetails = ({ product }) => {
+    const [index, setIndex] = useState(0);
+    const [quantity, setQuantity] = useState(1);
+    const dispatch = useDispatch();
+  return (
+    <Content>
+        <ImageBox>
+            <Slides>
+                <button 
+                onClick={() => index > 0 && setIndex((index) => index -1)}
+                disabled={index === 0}>
+                <i class="fas fa-chevron-left"></i>
+                </button>
+                <div>
+                    <img src={product.images[index]} alt="product" />
+                </div>
+                <button
+                onClick={() => 
+                index < product.images.length - 1 && setIndex((i) => i + 1)}
+                disabled={index === product.images.length -1}>
+                    <i class="fas fa-chevron-right"></i>
+                </button>
+            </Slides>
+            <PreviewImages active={index}>
+            <button 
+                onClick={() => index > 0 && setIndex((index) => index - 1)}
+                disabled={index === 0}>
+                <i class="fas fa-chevron-left"></i>
+            </button>   
+            <div>
+                {product.images.map((img, i) => (
+                    <img
+                    src={img}
+                    alt="previewproduct"
+                    onClick={() => setIndex(i)}
+                    key={i} />
+                ))}
+            </div> 
+            <button
+                onClick={() => 
+                index < product.images.length - 1 && setIndex((i) => i + 1)}
+                disabled={index === product.images.length -1}>
+                    <i class="fas fa-chevron-right"></i>
+                </button>  
+           </PreviewImages>
+        </ImageBox>
+        <Details>
+            <ProductName>{product.name}</ProductName>
+            <BrandName>{product.brand}</BrandName>
+            <PriceandRatings>
+                <div>
+                    <span>
+                        <span>%35</span> İndirim
+                    </span>
+                    <div>
+                        <span>1.699,00 TL</span>
+                        <span>{product.price} TL</span>
+                    </div>
+                </div>
+            </PriceandRatings>
+            <SellerRow>
+                <span>Satıcı: </span>
+                <span>{product.seller}</span>
+            </SellerRow>
+            <AddToCardRow>
+                <div>
+                    <button
+                    onClick={() =>
+                    quantity !== 1 && setQuantity((quantity) => quantity - 1)}>
+                    -
+                    </button>
+                    <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        textAlign: "center",
+                    }}>
+                    <span>{quantity}</span>
+                    <span>Adet</span>
+                    </div>
+                    <button
+                    onClick={() => setQuantity((quantity) => quantity + 1)}>
+                    +
+                    </button>
+                </div>
+                <button onClick={() => dispatch(AddToCart(product))}>
+            <i class="fas fa-shopping-cart"></i>
+            <span>Sepete Ekle</span>
+          </button>
+            </AddToCardRow>
+        </Details>
+    </Content>
+  );
+};
+
+export default ProductDetails;
