8e22b29cb69ed82438678d6b1a0d10688af4e759
Faik Turan <turanfaik@hotmail.com>
2022-10-31 19:24:13 +0300

    hepsiburada clone contunie

---
+++ hepsiburadaclone/src/Components/Products.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useRouteMatch } from "react-router-dom";
+import {
+  ProductsStyle,
+  Header,
+  Content,
+  OptionsCol,
+  ProductsCol,
+  ChildrenCategories,
+  Brands,
+} from "../Styles/Products.Style";
+import { ProductCard } from "./index";
+import { getCategoryProducts } from "../Store/actions/ProductsActions";
+import { getChildCategories } from "../Store/actions/CategoryActions";
+import NotFound from "./NotFound";
+
+const Products = () => {
+    const dispatch = useDispatch();
+    let { url } = useRouteMatch();
+
+    dispatch(getCategoryProducts(url.split("/")[2].replaceAll("-", " ")));
+    dispatch(getChildCategories(url.split("/")[2]));
+    const { products } = useSelector((state) => state.products);
+    const { category } = useSelector((state) => state.categories);
+
+    document.title = url.split("/")[2].replaceAll("-", " ");
+
+  return (
+    <ProductsStyle>
+        <Header>
+            <h1>{url.split("/")[2].replaceAll("-", " ")}</h1>
+            <div>
+                <span>Sıralama</span>
+                <i class="fas fa-arrow-down"></i>
+            </div>
+        </Header>
+        <Content>
+            <OptionsCol>
+                <ChildrenCategories>
+                    <div>
+                        <span>Kategori</span>
+                    </div>
+                    <div>
+                        {category.childs.map((subCategory) => {
+                            <span>{subCategory}</span>
+                        })}
+                    </div>
+                </ChildrenCategories>
+                <Brands>
+                    <div>
+                        <span>Marka</span>
+                    </div>
+                    <div>
+                        <div>
+                            <i class="fas fa-search"></i>
+                            <input type="text" placeholder="Filtrele" />
+                        </div>
+                        <div>
+                            {[1,1,1,1,1,1,1,1].map((x, index) => (
+                                <div key={index}>
+                                <input type="checkbox" id="check1" />
+                                <label for="check1">deneme</label>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </Brands>
+            </OptionsCol>
+            {products.length !== 0 ? (
+                <ProductsCol>
+                    {products.map((product, index) => (
+                        <ProductCard key={index} product={product} />
+                    ))}
+                </ProductsCol>
+            ) : (
+                <NotFound value={url.split("/")[2].replaceAll("-", " ")} />
+            ) }
+        </Content>
+    </ProductsStyle>
+  );
+};
+
+export default Products
