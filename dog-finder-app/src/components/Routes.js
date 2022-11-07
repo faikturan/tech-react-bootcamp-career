c071f62d745c2f146aa111530de0e789ba669fae
Faik Turan <turanfaik@hotmail.com>
2022-11-07 18:52:02 +0300

    dogfinderapp done

--- dog-finder-app/src/components/Routes.js
+++ dog-finder-app/src/components/Routes.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 
+import DogList from './DogList'
+import DogDetails from './DogDetails'
+
 export default class Routes extends Component {
   render() {
+    const getDog = props => {
+        const name = props.match.params.name
+        const currentDog = this.props.dogs.find(
+            dog => dog.name.toLowerCase() === name.toLowerCase()
+        )
+        return <DogDetails { ...props} dog={currentDog} />
+    }
     return (
-      <div>Routes</div>
+      <Switch>
+        <Route
+        exact
+        path="/dogs"
+        render={() => <DogList dogs={this.props.dogs}  />} /> 
+        <Route exact path="/dogs/:name" render={getDog} />
+        <Redirect to="/dogs" />
+      </Switch>
     )
   }
 }
