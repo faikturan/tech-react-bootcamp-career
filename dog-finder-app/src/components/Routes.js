623008d3e2ad9e93d5a18555adf338e91c74dc7e
Faik Turan <turanfaik@hotmail.com>
2022-11-07 18:30:12 +0300

    dogfinderapp contunie

---
+++ dog-finder-app/src/components/Routes.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Route, Switch, Redirect } from 'react-router-dom'
+
+export default class Routes extends Component {
+  render() {
+    return (
+      <div>Routes</div>
+    )
+  }
+}
